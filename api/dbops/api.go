fe331285e7b129d138ffb8ee10eaf77c08bb4e4a
EnJong <948162199@qq.com>
2019-03-10 23:34:29 +0800

    finish user api

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -5,23 +5,52 @@
  */
 package dbops
 
-import "database/sql"
-
-/**
- * 连接数据库
- */
-func openConn() *sql.DB {
-	return nil
-}
+import (
+	"database/sql"
+	"log"
+)
 
 func AddUserCredential(loginName, pwd string) error {
+	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")
+	if err != nil {
+		return err
+	}
+
+	if _, err := stmtIns.Exec(loginName, pwd); err != nil {
+		log.Println("insert user fail")
+	}
+
+	defer stmtIns.Close()
 	return nil
 }
 
 func GetUserCredential(loginName string) (string, error) {
-	return nil
+	stmtOut, err := dbConn.Prepare("SELECT pwd FROM user WHERE login_name = ?")
+	if err != nil {
+		log.Println("select user prepare fail")
+	}
+
+	var pwd string
+	if err := stmtOut.QueryRow(loginName).Scan(&pwd); err != nil && err != sql.ErrNoRows {
+		log.Println("query pwd fail")
+	}
+	defer stmtOut.Close()
+	return pwd, nil
 }
 
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM users WHERE login_name = ? AND pwd = ?")
+	if err != nil {
+		log.Printf("DeleteUser error: %s", err)
+		return err
+	}
 
+	if _, err := stmtDel.Exec(loginName, pwd); err != nil {
+		log.Println("delete user fail")
+	}
 
-
+	if err := stmtDel.Close(); err != nil {
+		log.Println("mysql connection close fail")
+	}
+	return nil
+}
